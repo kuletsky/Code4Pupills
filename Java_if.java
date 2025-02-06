a4a75b2b30724877aa3a654619135d940a5a04f2
kuletsky <kuletsky@gmail.com>
2025-02-06 09:32:11 -0500

    test

--- Java_if.java
+++ Java_if.java
@@ -11,24 +11,20 @@ public class Java_if {
         } while (i > 10);
         System.out.println(i);
 
-        
         int n = 0;
-        for (int k=5; k<=100; k++)
-        {
-            if (k%2==0 && k%5==0)
-            {
+        for (int k = 5; k <= 100; k++) {
+            if (k % 2 == 0 && k % 5 == 0) {
                 System.out.println(k);
                 n++;
-                if(n==3) 
+                if (n == 3)
                     break;
-                
+
             }
         }
 
         boolean required = true;
 
-        while (required) 
-        {
+        while (required) {
             System.out.println(required);
             required = false;
         }
--- redRover.js
+++ redRover.js
@@ -3,4 +3,4 @@ console.log('Hello world');
 a = 5;
 b = "10"
 
-console.log(a+b)
+console.log(a + b)
