3f1d7d52c1ba9655fd01d7f4b8656b4865340a93
kuletsky <kuletsky@gmail.com>
2025-02-14 23:05:10 -0500

    classes

--- redRover.js
+++ redRover.js
@@ -56,3 +56,33 @@ console.log(greet("Mike"))
 
 const name = "Mike"
 console.log(`Hello, <${name}> how are you doing today?`)
+
+
+
+function findMaxSumRow(matrix) {
+    let maxIndexRow = -1;
+    let maxSum = 0;
+
+    for (let i = 1; i < matrix.length; i++) {
+        let rowSum = 0;
+        for (let j = 1; j < matrix[i].length; j++) {
+            rowSum += matrix[i][j];
+        }
+        if (rowSum > maxSum) {
+            maxSum = rowSum;
+            maxIndexRow = i;
+        }
+    }
+
+    console.log(maxSum, maxIndexRow);
+}
+
+// Example usage:
+let matrix = [
+    [0, 0, 0, 0], 
+    [0, 3, 7, 1],  
+    [0, 9, 4, 2],  
+    [0, 5, 8, 6]
+];
+
+findMaxSumRow(matrix);
